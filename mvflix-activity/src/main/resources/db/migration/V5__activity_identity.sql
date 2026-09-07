7bd5ae676b38810ca97d29ba0b720e337d7498d2
Gabriel L. <gabrielliendo1257@gmail.com>
2026-09-06 22:46:30 -0500

    fix(activity): preserve Flyway migration history

--- mvflix-activity/src/main/resources/db/migration/V5__activity_identity.sql
+++ mvflix-activity/src/main/resources/db/migration/V5__activity_identity.sql
@@ -3,8 +3,7 @@ SET activity_key = 'ingestion:' || correlation_id
 WHERE activity_key IS NULL;
 
 ALTER TABLE activity_feed
-  DROP CONSTRAINT activity_feed_audience_id_correlation_id_key,
-  ALTER COLUMN activity_key SET NOT NULL;
+  DROP CONSTRAINT activity_feed_audience_id_correlation_id_key;
 
 CREATE UNIQUE INDEX activity_feed_identity_idx
   ON activity_feed(audience_id, activity_key);
---
+++ mvflix-activity/src/main/resources/db/migration/V6__activity_key_not_null.sql
@@ -0,0 +1,2 @@
+ALTER TABLE activity_feed
+  ALTER COLUMN activity_key SET NOT NULL;
