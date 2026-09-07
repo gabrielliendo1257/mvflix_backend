15f85eb7c45489366e76f64d3a24157bc05ce4f6
Gabriel L. <gabrielliendo1257@gmail.com>
2026-09-06 22:51:17 -0500

    chore(playback): bootstrap playback service

---
+++ mvflix-playback/src/main/java/com/gcorp/service/app/mvflix_playback/MvflixPlaybackApplication.java
@@ -0,0 +1,11 @@
+package com.gcorp.service.app.mvflix_playback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvflixPlaybackApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(MvflixPlaybackApplication.class, args);
+  }
+}
